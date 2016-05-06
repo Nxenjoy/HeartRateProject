da38b210485cc89d1dbedbfaecd2ba7a720dd69b
Joy <simple_joy@me.com>
2016-05-07 02:44:39 +0700

    Chart Complete (No customization)
    Chart Complete (No customization)

--- HealthCareProject/app/src/main/java/nl/s5630213023/healthcareproject/HeartRate/exercise/Exercise2.java
+++ HealthCareProject/app/src/main/java/nl/s5630213023/healthcareproject/HeartRate/exercise/Exercise2.java
@@ -9,6 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import java.util.ArrayList;
 
 import nl.s5630213023.healthcareproject.R;
@@ -18,6 +26,12 @@ public class Exercise2 extends Fragment {
     ListView listView;
     static ArrayList<Exer> arrayExercise = new ArrayList<Exer>();
     ExerciseAdapter adapter;
+
+    //chart
+    private BarChart mChart;
+    ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+    ArrayList<String> xVals = new ArrayList<String>();
+
 
     public static Exercise2 newInstance() {
         return new Exercise2();
@@ -43,15 +57,67 @@ public class Exercise2 extends Fragment {
         registerForContextMenu(listView);
         btnShow();
 
+        //chart
+        mChart = (BarChart) v.findViewById(R.id.chartEx);
+
+        mChart.setDescription("");
+
+        mChart.setMaxVisibleValueCount(60);
+
+        // scaling can now only be done on x- and y-axis separately
+        mChart.setPinchZoom(false);
+
+        mChart.setDrawBarShadow(false);
+        mChart.setDrawGridBackground(false);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setSpaceBetweenLabels(0);
+        xAxis.setDrawGridLines(false);
+
+        mChart.getAxisLeft().setDrawGridLines(false);
+
+        mChart.animateY(2500);
+
+        mChart.getLegend().setEnabled(false);
+
+        setData(20, 30);
+
         return v;
+    }
+
+    private void setData(int count, float range) {
+
+        BarDataSet set1;
+
+        if (mChart.getData() != null &&
+                mChart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet)mChart.getData().getDataSetByIndex(0);
+
+            mChart.getData().notifyDataChanged();
+            mChart.notifyDataSetChanged();
+        } else {
+            set1 = new BarDataSet(yVals1, "Data Set");
+            set1.setColors(ColorTemplate.VORDIPLOM_COLORS);
+            set1.setDrawValues(false);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(xVals, dataSets);
+
+            mChart.setData(data);
+            mChart.invalidate();
+        }
     }
 
     private void btnShow() {
         Uri u = Uri.parse("content://ExerCiseDB");
         Cursor c = getActivity().getContentResolver().query(u, null, null, null, null);
         while (c.moveToNext()) {
-            arrayExercise.add(new Exer(c.getInt(0), c.getString(1).toString(), c.getString(2).toString(), c.getInt(3) ,c.getString(4).toString()));
-
+            arrayExercise.add(new Exer(c.getInt(0), c.getString(1).toString(), c.getString(2).toString(), c.getInt(3), c.getString(4).toString()));
+            xVals.add(c.getString(2));
+            yVals1.add(new BarEntry(c.getInt(3),c.getInt(0)));
         }
     }
 }
